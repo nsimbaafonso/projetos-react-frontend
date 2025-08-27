10bc0dcd134aaa39edb3f10482791eef0c35c471
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-27 14:30:09 +0100

    Configuração do AOS

--- restaurante/src/main.jsx
+++ restaurante/src/main.jsx
@@ -6,6 +6,15 @@ import AppRoutes from "./routes/AppRoutes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// inicializar o AOS
+AOS.init({
+  duration: 1000,
+  once: true,
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
