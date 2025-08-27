e5e7e1cbff5bbbcb9e396b1800a7df760f255acd
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-27 13:52:55 +0100

    Componente com dados do cardápio

---
+++ restaurante/src/components/DadosCardapio.js
@@ -0,0 +1,16 @@
+import prato1 from "../assets/img/prato-1.jpg";
+import prato2 from "../assets/img/prato-2.jpg";
+import prato3 from "../assets/img/prato-3.jpg";
+import prato4 from "../assets/img/prato-4.jpg";
+import prato5 from "../assets/img/prato-5.jpg";
+import prato6 from "../assets/img/prato-6.jpg";
+
+const cardapios = [
+    {id: 1, imagem: prato1, titulo: "Prato Especial", desc: "Uma descrição deliciosa do prato.", preco: 500, whatsapp: "https://wa.me/244923456789?text=Olá!%20Gostaria%20de%20pedir%20o%20Prato%20Especial."},
+    {id: 2, imagem: prato2, titulo: "Prato Especial", desc: "Uma descrição deliciosa do prato.", preco: 700, whatsapp: "https://wa.me/244923456789?text=Olá!%20Gostaria%20de%20pedir%20o%20Prato%20Especial."},
+    {id: 3, imagem: prato3, titulo: "Prato Especial", desc: "Uma descrição deliciosa do prato.", preco: 900, whatsapp: "https://wa.me/244923456789?text=Olá!%20Gostaria%20de%20pedir%20o%20Prato%20Especial."},
+    {id: 4, imagem: prato4, titulo: "Prato Especial", desc: "Uma descrição deliciosa do prato.", preco: 1100, whatsapp: "https://wa.me/244923456789?text=Olá!%20Gostaria%20de%20pedir%20o%20Prato%20Especial."},
+    {id: 5, imagem: prato5, titulo: "Prato Especial", desc: "Uma descrição deliciosa do prato.", preco: 1300, whatsapp: "https://wa.me/244923456789?text=Olá!%20Gostaria%20de%20pedir%20o%20Prato%20Especial."},
+    {id: 6, imagem: prato6, titulo: "Prato Especial", desc: "Uma descrição deliciosa do prato.", preco: 1500, whatsapp: "https://wa.me/244923456789?text=Olá!%20Gostaria%20de%20pedir%20o%20Prato%20Especial."},
+]
+export default cardapios
