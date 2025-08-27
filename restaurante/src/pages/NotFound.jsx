61e8c02deaa32d8730bfec4d517227091e81c1d7
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-27 12:51:37 +0100

    Primeiros passos no rodapé

--- restaurante/src/pages/NotFound.jsx
+++ restaurante/src/pages/NotFound.jsx
@@ -5,13 +5,22 @@ import Footer from "../components/Footer";
 export default function NotFound() {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>404 - Página não encontrada</title>
         <meta name="description" content="Página não encontrada." />
-    </Helmet>
-    
-    <Header />
-    <Footer />
+      </Helmet>
+
+      <Header />
+      <section className="d-flex align-items-center justify-content-center bg-light py-2"style={{ height: '100vh' }}>
+        <div className="text-center">
+          <h1 className="display-1 fw-bold">404</h1>
+          <p className="lead mb-4">A página que você procura não foi encontrada.</p>
+          <button className="btn btn-lg btn-warning" onClick={() => window.history.back()}>
+            Voltar
+          </button>
+        </div>
+      </section>
+      <Footer />
     </>
   );
 }
