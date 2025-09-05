803888c35dcf2a51af2a100189f36281f691c448
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-09-05 22:31:45 +0100

    Importações no main do restaurante atualizado

--- restaurante/src/main.jsx
+++ restaurante/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -15,10 +15,10 @@ AOS.init({
   once: true,
 });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
